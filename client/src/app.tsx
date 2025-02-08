b026b45b4793d6c86e021aa1edbafd2500c7148a
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2025-02-07 23:36:59 -0500

    Added code for the frontend

---
+++ client/src/app.tsx
@@ -0,0 +1,10 @@
+import Wildfire from './component/wildfire.tsx';
+import './index.css';
+
+export default function App() {
+  return (
+    <div className={'h-screen flex flex-col'}>
+      <Wildfire/>
+    </div>
+  )
+}
